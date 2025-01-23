a38eee358b644b413003d8b57a99b73cf2165823
nitinkumarpals <nitinpal900@gmail.com>
2025-01-23 21:38:11 +0530

    started frontend

---
+++ apps/web/src/app/page.tsx
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-3xl flex h-screen w-screen items-center justify-center">Home Page</h1>
+     
+    </div>
+  );
+}
